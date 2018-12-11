3520f89d1786733c78b488cffffc74baedf60fc3
dingdaojun <dingdaojun@smartdevices.com.cn>
2018-12-11 23:58:52 +0800

    feat |> Add Carthage support.

---
+++ HMCacheDemo/HMCache/HMCache.h
@@ -0,0 +1,25 @@
+//
+//  HMCache.h
+//  HMCache
+//
+//  Created by dingdaojun on 2018/12/4.
+//  Copyright © 2018 dingdaojun@huami.com All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HMCache.
+FOUNDATION_EXPORT double HMCacheVersionNumber;
+
+//! Project version string for HMCache.
+FOUNDATION_EXPORT const unsigned char HMCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMCache/PublicHeader.h>
+
+#import <HMCache/HMKeyMaker.h>
+#import <HMCache/HMObject.h>
+#import <HMCache/HMObject+Cache.h>
+#import <HMCache/HMCacheManager.h>
+#import <HMCache/HMCacheManager+Array.h>
+#import <HMCache/HMCacheManager+Number.h>
+#import <HMCache/HMMigrationData.h>
